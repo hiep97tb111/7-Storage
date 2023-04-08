358d64f72a42aee6b2679f5c96eab8ee0186b425
HiepPD <hieppdutck59@gmail.com>
2023-04-08 09:35:38 +0700

    When capture image finish then show app use URI (Save & Read ExternalStorage)

--- app/src/main/java/com/example/storagedemo/ImageAct.kt
+++ app/src/main/java/com/example/storagedemo/ImageAct.kt
@@ -3,6 +3,7 @@ package com.example.storagedemo
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.os.Environment.DIRECTORY_DOCUMENTS
@@ -68,7 +69,8 @@ class ImageAct : AppCompatActivity() {
 
             // save External Storage
             val pathNameFolder = "ManchesterUnited"
-            val file = File(Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS).path+"/${pathNameFolder}", "my_image.jpg")
+            val nameImage = "my_image.jpg"
+            val file = File(Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS).path+"/${pathNameFolder}", nameImage)
             val imageBitmap = result.data?.extras?.get("data") as Bitmap
             val outputStream: OutputStream = FileOutputStream(file)
             imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
@@ -76,9 +78,11 @@ class ImageAct : AppCompatActivity() {
             outputStream.close()
 
             // Show image in app use Bitmap
-            // If use uri then null => Solution: Save Image ExternalStorage and later read Image ExternalStorage
-            Log.e("Logger", imageBitmap.toString())
-            imvCaptureImage.setImageBitmap(imageBitmap)
+//            imvCaptureImage.setImageBitmap(imageBitmap)
+
+            // Show image in app use Uri
+            Log.e("Logger", Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS).path+"/${pathNameFolder}/${nameImage}")
+            imvCaptureImage.setImageURI(Uri.parse(Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS).path+"/${pathNameFolder}/${nameImage}"))
         }
     }
 }
