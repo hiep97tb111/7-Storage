12dfbe600e3d357d8d2f545d5ebd1dcc5c7232e8
HiepPD <hieppdutck59@gmail.com>
2023-04-08 08:54:05 +0700

    When capture image finish then show app and save ExternalStorage (DIRECTORY_DOCUMENTS or Folder in DIRECTORY_DOCUMENTS)

--- app/src/main/java/com/example/storagedemo/ImageAct.kt
+++ app/src/main/java/com/example/storagedemo/ImageAct.kt
@@ -1,13 +1,26 @@
 package com.example.storagedemo
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.Environment
+import android.os.Environment.DIRECTORY_DOCUMENTS
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
-class ImageAct: AppCompatActivity() {
+class ImageAct : AppCompatActivity() {
+    private lateinit var imvCaptureImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_image)
@@ -17,22 +30,55 @@ class ImageAct: AppCompatActivity() {
 
     private fun initViews() {
 
+        imvCaptureImage = findViewById(R.id.imvCaptureImage)
+
         findViewById<TextView>(R.id.tvCreateFolder).setOnClickListener {
             handleCreateFolder()
         }
 
+        findViewById<TextView>(R.id.tvCaptureImage).setOnClickListener {
+            handleCaptureImage()
+        }
+
+    }
+
+    private fun handleCaptureImage() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startForResultCaptureImage.launch(cameraIntent)
     }
 
     private fun handleCreateFolder() {
         // create Folder in Environment Documents
         val pathNameFolder = "ManchesterUnited"
-        val folder = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).path+"/"+pathNameFolder)
+        val folder = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).path + "/" + pathNameFolder)
 
-        if(folder.exists()){
+        if (folder.exists()) {
             Toast.makeText(this, "Folder Exists", Toast.LENGTH_SHORT).show()
-        }else{
+        } else {
             folder.mkdir()
             Toast.makeText(this, "Folder Not Exists", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val startForResultCaptureImage = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        //  you will get result here in result.data
+        if (result.resultCode == Activity.RESULT_OK) {
+
+            // save External Storage
+            val pathNameFolder = "ManchesterUnited"
+            val file = File(Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS).path+"/${pathNameFolder}", "my_image.jpg")
+            val imageBitmap = result.data?.extras?.get("data") as Bitmap
+            val outputStream: OutputStream = FileOutputStream(file)
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream.flush()
+            outputStream.close()
+
+            // Show image in app use Bitmap
+            // If use uri then null => Solution: Save Image ExternalStorage and later read Image ExternalStorage
+            Log.e("Logger", imageBitmap.toString())
+            imvCaptureImage.setImageBitmap(imageBitmap)
         }
     }
 }
--- app/src/main/java/com/example/storagedemo/MainAct.kt
+++ app/src/main/java/com/example/storagedemo/MainAct.kt
@@ -1,16 +1,37 @@
 package com.example.storagedemo
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainAct : AppCompatActivity() {
+    private val requestCodePermission: Int = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_main)
 
         initViews()
+
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Grant Permission Success", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    android.Manifest.permission.CAMERA
+                ), requestCodePermission)
+        }
     }
 
     private fun initViews() {
